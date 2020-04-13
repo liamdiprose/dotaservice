85fd7df213e1ba589ef2819ed913599ddf51fcb3
Liam Diprose <liam@liamdiprose.com>
2020-04-13 21:04:27 +1200

    Add example and dependency managemnet

--- examples/move_to_location.py
+++ examples/move_to_location.py
@@ -2,23 +2,97 @@
 
 import asyncio
 
-from grpc import Channel
-from protobuf.DotaService_grpc import DotaServiceStub
-from protobuf.DotaService_pb2 import Action
-from protobuf.DotaService_pb2 import Config
+from grpclib.client import Channel
+
+from dotaservice.protos.DotaService_grpc import DotaServiceStub
+from dotaservice.protos.DotaService_pb2 import Actions, HeroPick, Team, Hero, HeroControlMode, ObserveConfig, HostMode
+from dotaservice.protos.DotaService_pb2 import GameConfig
+from dotaservice.protos.dota_gcmessages_common_bot_script_pb2 import CMsgBotWorldState
 
 
 async def main():
+    action = Actions(
+        actions=CMsgBotWorldState.Actions(
+            actions=[
+                CMsgBotWorldState.Action(
+                    actionType = CMsgBotWorldState.Action.Type.DOTA_UNIT_ORDER_MOVE_TO_POSITION,
+                    moveToLocation = CMsgBotWorldState.Action.MoveToLocation(
+                        location=CMsgBotWorldState.Vector(x=-394, y=-486, z=2)
+                    )
+                )
+        ]),
+        team_id=Team.TEAM_RADIANT
+    )
     # Connect to the DotaService.
-    env = DotaServiceStub(Channel('127.0.0.1', 13337))
+    env = DotaServiceStub(Channel('192.168.1.17', 13337))
 
     # Get the initial observation.
-    observation = await env.reset(Config())
-    for i in range(8):
+    observation = await env.reset(GameConfig(
+        host_mode=HostMode.HOST_MODE_DEDICATED,
+        hero_picks=[
+            HeroPick(
+                team_id=Team.TEAM_RADIANT, 
+                hero_id=Hero.NPC_DOTA_HERO_PUDGE,
+                control_mode=HeroControlMode.HERO_CONTROL_MODE_CONTROLLED
+                ),
+            HeroPick(
+                team_id=Team.TEAM_RADIANT, 
+                hero_id=Hero.NPC_DOTA_HERO_PUDGE,
+                control_mode=HeroControlMode.HERO_CONTROL_MODE_DEFAULT
+                ),
+            HeroPick(
+                team_id=Team.TEAM_RADIANT, 
+                hero_id=Hero.NPC_DOTA_HERO_PUDGE,
+                control_mode=HeroControlMode.HERO_CONTROL_MODE_DEFAULT
+                ),
+            HeroPick(
+                team_id=Team.TEAM_RADIANT, 
+                hero_id=Hero.NPC_DOTA_HERO_PUDGE,
+                control_mode=HeroControlMode.HERO_CONTROL_MODE_DEFAULT
+                ),
+            HeroPick(
+                team_id=Team.TEAM_RADIANT, 
+                hero_id=Hero.NPC_DOTA_HERO_PUDGE,
+                control_mode=HeroControlMode.HERO_CONTROL_MODE_DEFAULT
+                ),
+            HeroPick(
+                team_id=Team.TEAM_DIRE, 
+                hero_id=Hero.NPC_DOTA_HERO_PUDGE,
+                control_mode=HeroControlMode.HERO_CONTROL_MODE_DEFAULT
+                ),
+            HeroPick(
+                team_id=Team.TEAM_DIRE, 
+                hero_id=Hero.NPC_DOTA_HERO_PUDGE,
+                control_mode=HeroControlMode.HERO_CONTROL_MODE_DEFAULT
+                ),
+            HeroPick(
+                team_id=Team.TEAM_DIRE, 
+                hero_id=Hero.NPC_DOTA_HERO_PUDGE,
+                control_mode=HeroControlMode.HERO_CONTROL_MODE_DEFAULT
+                ),
+            HeroPick(
+                team_id=Team.TEAM_DIRE, 
+                hero_id=Hero.NPC_DOTA_HERO_PUDGE,
+                control_mode=HeroControlMode.HERO_CONTROL_MODE_DEFAULT
+                ),
+            HeroPick(
+                team_id=Team.TEAM_DIRE, 
+                hero_id=Hero.NPC_DOTA_HERO_PUDGE,
+                control_mode=HeroControlMode.HERO_CONTROL_MODE_DEFAULT
+                ),
+        ],
+        ticks_per_observation=30
+        ))
+
+    print(f"moving {action}")
+
+    await env.act(action)
+    while True:
         # Sample an action from the action protobuf
-        action = Action.MoveToLocation(x=2, y=2, z=2)
         # Take an action, returning the resulting observation.
-        observation = await env.step(action)
+
+        observation = await env.observe(ObserveConfig(team_id=Team.TEAM_RADIANT))
+        print(observation)
 
 if __name__ == '__main__':
     asyncio.run(main())
